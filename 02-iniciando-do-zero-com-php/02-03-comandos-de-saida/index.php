8709573defac4a80bf0a711b28f353ad270dfd86
Tarcisio Couto <tarcisio.pereira@caruaru.ifpe.edu.br>
2019-12-07 14:44:19 -0300

    :tada: Primeiro commit

---
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -0,0 +1,40 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.03 - Comandos de saída");
+
+/**
+ * [ echo ] https://php.net/manual/pt_BR/function.echo.php
+ */
+fullStackPHPClassSession("echo", __LINE__);
+
+echo "<p>Olá Mundo!", " ". "<span class='tag'>#BoraProgramar!</span>", "</p>";
+
+
+/**
+ * [ print ] https://php.net/manual/pt_BR/function.print.php
+ */
+fullStackPHPClassSession("print", __LINE__);
+
+
+/**
+ * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
+ */
+fullStackPHPClassSession("print_r", __LINE__);
+
+
+/**
+ * [ printf ] https://php.net/manual/pt_BR/function.printf.php
+ */
+fullStackPHPClassSession("printf", __LINE__);
+
+
+/**
+ * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
+ */
+fullStackPHPClassSession("vprintf", __LINE__);
+
+
+/**
+ * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
+ */
+fullStackPHPClassSession("var_dump", __LINE__);
