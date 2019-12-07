58668c161a1ebdd56ba89346d9371af158f3f552
Tarcisio Couto <tarcisio.pereira@caruaru.ifpe.edu.br>
2019-12-07 16:08:11 -0300

    :rocket: Finalização 03-02 funções para strings

--- 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
+++ 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
@@ -11,8 +11,20 @@ $string = "O último show do AC/DC foi incrível!";
 
 var_dump([
     "string" => $string,
+    /**
+     * [strlen] Função que conta os caracteres. Porém, cuidado ao utilizar, pois
+     * os acentos são considerados como caracteres. 
+     */
     "strlen" => strlen($string),
+    /**
+     * [mb_strlen] Função que conta os caracteres. Os acentos NÃO são considerados
+     * durante o uso desta função.
+     */
     "mb_strlen" => mb_strlen($string),
+    "substr"    => substr($string, 9),
+    "mb_substr"    => mb_substr($string, 9),
+    "strtoupper"    => strtoupper($string),
+    "mb_strtoupper"    => mb_strtoupper($string),
 ]);
 
 
@@ -21,14 +33,63 @@ var_dump([
  */
 fullStackPHPClassSession("conversão de caixa", __LINE__);
 
+$mbString = $string;
+var_dump([
+    "mb_strtoupper" => mb_strtoupper($mbString),
+    "mb_strtolower" => mb_strtolower($mbString),
+    "mb_convert_case UPPER" => mb_convert_case($mbString, MB_CASE_UPPER),
+    "mb_convert_case LOWER" => mb_convert_case($mbString, MB_CASE_LOWER),
+    "mb_convert_case TITLE" => mb_convert_case($mbString, MB_CASE_TITLE),
+]);
+
 
 /**
  * [ substituição ] multibyte and replace
  */
 fullStackPHPClassSession("substituição", __LINE__);
 
+$mbReplace = $mbString . " Fui, iria novamente, e foi épico!";
+
+var_dump([
+    "mb_strlen" => mb_strlen($mbReplace),
+    "mb_strpos" => mb_strpos($mbReplace, ", "),
+    "mb_strrpos" => mb_strrpos($mbReplace, ", "),
+    "mb_substr" => mb_substr($mbReplace, 40 + 2, 14),
+    "mb_strstr" => mb_strstr($mbReplace, ", ", true),
+    "mb_strrchr" => mb_strrchr($mbReplace, ", ", true),
+]);
+
+$mbReplace = $string;
+
+echo "<p>", $string, "</p>";
+echo "<p>", str_replace("AC/DC", "Nirvana", $mbReplace), "</p>";
+echo "<p>", str_replace(["AC/DC", "incrível"], ["Nirvana", "ÉPICO"], $mbReplace), "</p>";
+
+$article = <<<ROCK
+    <article>
+        <h3>event</h3>
+        <p>desc</p>
+    </article>
+ROCK;
+
+$articleData = [
+    "event" => "Rock in Rio",
+    "desc"  => $mbReplace
+];
+
+echo str_replace(array_keys($articleData), array_values($articleData), $article);
+
 
 /**
  * [ parse string ] parse_str | mb_parse_str
  */
 fullStackPHPClassSession("parse string", __LINE__);
+
+$endPoint = "name=Robson&email=cursos@upinside.com.br";
+mb_parse_str($endPoint, $parseEndPoint);
+
+var_dump([
+    $endPoint,
+    $parseEndPoint,
+    (object)$parseEndPoint
+]);
